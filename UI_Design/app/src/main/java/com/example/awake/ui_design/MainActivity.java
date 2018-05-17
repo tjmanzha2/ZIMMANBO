c7fefb1669641f0d7f82a16a40f08fec5f42bb22
tjmanzha2 <cjj9327@naver.com>
2018-05-17 23:02:25 +0900

    First Activity Design_ing

---
+++ UI_Design/app/src/main/java/com/example/awake/ui_design/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.awake.ui_design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view) {
+
+    }
+
+    public void signUp(View view) {
+
+    }
+}
